8a0469e4e198f3fb068eb22a634ddb5f76d20d00
Josh Stroud <122675906+joshagilend@users.noreply.github.com>
2024-11-23 22:04:09 -0800

    Create js.js

---
+++ app/js.js
@@ -0,0 +1,9 @@
+// Simulate generating a large number of lines
+function generateLines() {
+    for (let i = 0; i < 1e9; i++) { // Adjust loop for practicality
+        console.log(`Line ${i}: console.log('Hello World');`);
+    }
+}
+
+// Uncomment to run (be cautious with memory usage!)
+// generateLines();
