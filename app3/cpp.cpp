6906c99522481da23c82cf6f54141c0c5fe23d91
Josh Stroud <122675906+joshagilend@users.noreply.github.com>
2024-11-23 22:11:42 -0800

    Create cpp.cpp

---
+++ app3/cpp.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    for (int i = 0; i < 1000000; i++) {
+        cout << "Line " << i << ": cout << 'Hello World' << endl;" << endl;
+    }
+    return 0;
+}
